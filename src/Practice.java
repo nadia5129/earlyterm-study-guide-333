2b968f6a64619f496147f820c2f54d57447f8334
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-04-19 15:29:51 -0700

    completed max diff questions in array

--- src/Practice.java
+++ src/Practice.java
@@ -7,7 +7,19 @@ public class Practice {
      */
     public static int maxDiff(int[] nums) {
         // TODO: implement this
-        return -1;
+        int min = nums[0];
+        int max= nums[0];
+
+        for( int i = 0; i < nums.length; i++){
+            if(nums [i] < min){
+                min = nums[i];
+            }
+            if(nums [i] > max){
+                max = nums[i];
+            }
+            
+        }
+        return max - min;
     }
 
 
